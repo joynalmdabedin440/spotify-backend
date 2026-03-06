933b7df1461472329b35f78db3300e7b7ee62479
joynalmdabedin <joynalmdabedin440@gmail.com>
2026-03-06 23:33:32 +0600

    add user schema

---
+++ src/controllers/auth.controller.js
@@ -0,0 +1,5 @@
+
+async function createUser(req,res) {
+    const user = req.body.user 
+    
+}
---
+++ src/models/user.model.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose")
+
+const userSchema = mongoose.Schema({
+    username: {
+        type: String,
+        unique: true,
+        require:true
+       
+    },
+    email: {
+        type: String,
+        unique: true,
+        require:true
+       
+    },
+    password: {
+        type: String,
+        require:true
+    },
+    role: {
+        type: String,
+        enum: ['user', 'artist'],
+        default:'user'
+    }
+})
+
+const userModel = mongoose.model("user", userSchema)
+
+module.exports= userModel
