3e2d0dfe57ed9f15a048f5997b850e94ecab30fb
joynalmdabedin <joynalmdabedin440@gmail.com>
2026-03-08 23:42:56 +0600

    add auth route

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,4 +1,8 @@
 const express = require('express');
 const router = express.Router()
+const authController = require("../controllers/auth.controller")
 
-router.post("/register",)
+router.post("/register",authController)
+
+
+module.exports = router
