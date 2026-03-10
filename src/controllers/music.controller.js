1582dfd29b0898a7c2ca5f186c6e62a33e9fc9ca
joynalmdabedin <joynalmdabedin440@gmail.com>
2026-03-10 22:44:07 +0600

    add album model

--- src/controllers/music.controller.js
+++ src/controllers/music.controller.js
@@ -3,10 +3,66 @@ const uploadFile = require('../services/storage.service')
 
 const jwt = require("jsonwebtoken")
 
+// async function createMusic(req, res) {
+//     const { title } = req.body
+//     const file = req.file
+
+
+//     const token = req.cookies.token
+
+//     if (!token) {
+//         return res.status(401).json({
+//             msg: "unauthorize user"
+//         })
+//     }
+
+//     try {
+//         const decoded = await jwt.verify(token, process.env.JWT_SECRET)
+
+//         if (decoded.role !== "artist") {
+//             return res.status(403).json({
+//                 msg: "Forbidden access"
+//             })
+//         }
+
+//         const result = await uploadFile(file.buffer.toString("base64"))
+
+//         const music = await musicModel.create({
+//             title,
+//             uri: result.url,
+//             artist: decoded.id
+
+
+//         })
+
+//         res.status(201).json({
+//             msg: "music created successfully",
+//             music: {
+//                 id: music._id,
+//                 uri: music.uri,
+//                 title: music.title,
+//                 artist: music.artist
+//             }
+//         })
+
+
+
+//     } catch (error) {
+
+//         return res.status(301).json({
+//             msg: "This user not allow to create music"
+//         })
+
+//     }
+
+
+
+
+// }
+
 async function createMusic(req, res) {
     const { title } = req.body
     const file = req.file
-
 
     const token = req.cookies.token
 
@@ -21,22 +77,20 @@ async function createMusic(req, res) {
 
         if (decoded.role !== "artist") {
             return res.status(403).json({
-                msg: "Forbidden access"
+                msg: "Forbidden Access"
             })
         }
 
-        const result = await uploadFile(file.buffer.toString("base64"))
+        const result = await uploadFile(file.buffer.toString('base64'))
 
         const music = await musicModel.create({
-            title,
             uri: result.url,
+            title,
             artist: decoded.id
-
-
         })
 
         res.status(201).json({
-            msg: "music created successfully",
+            msg: "Music created successfully",
             music: {
                 id: music._id,
                 uri: music.uri,
@@ -46,11 +100,9 @@ async function createMusic(req, res) {
         })
 
 
-
     } catch (error) {
-
-        return res.status(301).json({
-            msg: "This user not allow to create music"
+        return res.status(401).json({
+            msg: "unauthorize user"
         })
 
     }
@@ -58,58 +110,20 @@ async function createMusic(req, res) {
 
 
 
+
 }
 
-// async function createMusic(req, res) {
-//     const { title } = req.body
-//     const file = req.file
-
-//     const token = req.cookies.token
-
-//     if (!token) {
-//         return res.status(401).json({
-//             msg:"unauthorize user"
-//         })
-//     }
-
-//     try {
-//         const decoded = await jwt.verify(token, process.env.JWT_SECRET)
-
-//         if (decoded.role !== "artist") {
-//             return res.status(403).json({
-//                 msg:"Forbidden Access"
-//             })
-//         }
-
-
-//     } catch (error) {
-//         return res.status(401).json({
-//             msg:"unauthorize user"
-//         })
-
-//     }
-
-//     const result = await uploadFile(file.buffer.toString('base64'))
-
-//     const music = await musicModel.create({
-//         uri: result.url,
-//         title,
-//         artist:token.id
-//     })
-
-//     res.status(201).json({
-//         msg: "Music created successfully",
-//         music: {
-//             id: music._id,
-//             uri: music.uri,
-//             title: music.tile,
-//             artist:music.artist
-//         }
-//     })
-
-
-
-// }
-
+// album controller
+async function createAlbum(req, res) {
+    const token = req.cookies.token
+    if (!token) {
+      
+        return res.status(401).json({
+            msg:"Unauthorized User"
+        })
+    
+  }  
+    
+}
 
 module.exports = { createMusic }
